b132650b4e4fb8c2bdba2bae4e32bf1ef3b32a5f
Michael5644 <mc5644@rit.edu>
2025-12-01 13:09:11 -0500

    Updated

---
+++ ServerDownloads.ps1
@@ -0,0 +1,2 @@
+Install-WindowsFeature -Name AD-DomainServices, DNS, DHCP -IncludeManagementTools
+winget --install Git.Git
--- ServerSetup.ps1
+++ ServerSetup.ps1
@@ -1,9 +1,8 @@
-Install-WindowsFeature -Name AD-DomainServices, DNS, DHCP -IncludeManagementTools
-
 $name = Read-Host "Enter Domain Name: "
 $netbiosName = Read-Host "Enter Net BIOS Name: "
 
 Import-Module ADDSDeployment
+Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\WINDOWS\NTDS" `
 -DomainMode "Win2025" `
