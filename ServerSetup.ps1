8f857dc798894f935a9714f9d104b183fd791857
Michael5644 <mc5644@rit.edu>
2025-12-01 13:02:30 -0500

    Updated

--- ServerSetup.ps1
+++ ServerSetup.ps1
@@ -4,7 +4,6 @@ $name = Read-Host "Enter Domain Name: "
 $netbiosName = Read-Host "Enter Net BIOS Name: "
 
 Import-Module ADDSDeployment
-Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\WINDOWS\NTDS" `
 -DomainMode "Win2025" `
