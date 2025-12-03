bcb444b9f1ef34bcd261dbd9ed09133c18904460
Michael5644 <mc5644@rit.edu>
2025-12-03 13:09:34 -0500

    Updated.

--- dhcpSetup.ps1
+++ dhcpSetup.ps1
@@ -2,6 +2,7 @@ $name = Read-Host "Enter scope name"
 $scopeStart = Read-Host "Enter Scope Start IP"
 $scopeEnd = Read-Host "Enter Scope End IP"
 $subnetMask = Read-Host "Enter Subnet Mask"
+
 # Create DHCP Scope
 Add-DhcpServerv4Scope -Name $name -StartRange $scopeStart -EndRange $scopeEnd -SubnetMask $subnetMask
 
