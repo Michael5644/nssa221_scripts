222d4ba2243cd755e4aea8f41585127db7a6d49f
Michael5644 <mc5644@rit.edu>
2025-12-05 15:24:27 -0500

    Updated

--- ServerSetup.ps1
+++ ServerSetup.ps1
@@ -1,7 +1,7 @@
 Install-WindowsFeature -Name AD-Domain-Services, DNS, DHCP -IncludeManagementTools
 
-$name = Read-Host "Enter Domain Name"
-$netbiosName = Read-Host "Enter Net BIOS Name"
+$name = Read-Host "Enter Domain Name" # Example: mc5644.com. NOT the full FQDN which is <hostname>.<rit-username>.com
+$netbiosName = Read-Host "Enter Net BIOS Name" # Basically domain name but in all caps, without the .com
 
 Import-Module ADDSDeployment
 Install-ADDSForest `
