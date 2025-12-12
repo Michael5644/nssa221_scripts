32bc4790dd17ba66b481023f2e0399f1393f1900
Michael5644 <mc5644@rit.edu>
2025-12-12 08:34:48 -0500

    Updated

--- ServerSetup.ps1
+++ ServerSetup.ps1
@@ -1,7 +1,7 @@
 Install-WindowsFeature -Name AD-Domain-Services, DNS, DHCP, GPMC -IncludeManagementTools
 
 $name = Read-Host "Enter Domain Name" # Example: mc5644.com. NOT the full FQDN which is <hostname>.<rit-username>.com
-# $netbiosName = Read-Host "Enter Net BIOS Name" # Basically domain name but in all caps, without the .com
+$netbiosName = Read-Host "Enter Net BIOS Name" # Basically domain name but in all caps, without the .com
 
 Import-Module ADDSDeployment
 Install-ADDSForest `
@@ -9,7 +9,7 @@ Install-ADDSForest `
 -DatabasePath "C:\WINDOWS\NTDS" `
 -DomainMode "Win2025" `
 -DomainName "$name" `
-# -DomainNetbiosName "$netbiosName" `
+-DomainNetbiosName "$netbiosName" `
 -ForestMode "Win2025" `
 -InstallDns:$true `
 -LogPath "C:\WINDOWS\NTDS" `
